99ed77612a28b979551c069f5654606145c57877
ericmp <ericmorenopou@gmail.com>
2022-12-16 00:01:32 +0100

    feat: update structure

--- main.js
+++ main.js
@@ -1,13 +1,102 @@
-const agregarFila = () => {
-  document.getElementById("headtable").insertRow(-1).innerHTML =
-    '<td> <input type="text"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> <input class="check" type="checkbox"> </td><td> </td><td></td><td></td><td></td>'
-  /* MATRICULA                        MATERIAL SOLICITADO                            SE HA RESPETADO EL PLAZO                       CORRESPONDIENTE AL PEDIDO                           MANIPULADOR CORRECTO                           NOMBRE FABRICANTE                               TIENE HOMOLGACION                                TIENE ESCUDOS                                  PLACA RALLADA                                  DISEÑO CORRECTO                                  REMACHES CORRECTOS */
+const myTable = document.querySelector('#myTable')
+const tableBody = myTable.tBodies[0]
+
+const addRow = () => {
+  const tr = document.createElement('tr')
+
+  tr.innerHTML = `
+    <td>
+      <button>
+        ❌
+      </button>
+    </td>
+
+    <td>
+      <input type="text">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+    
+    <td>
+      <select>
+        <option value="John">John</option>
+        <option value="Bruno">Bruno</option>
+        <option value="Eduard">Eduard</option>
+        <option value="Efka">Efka</option>
+      </select>
+    </td>
+    
+    <td>
+      <div>
+        <input type="radio" id="aluminio" name="material" value="aluminio" checked>
+        <label for="aluminio">Aluminio</label>
+      </div>
+
+      <div>
+        <input type="radio" id="acrilica" name="material" value="acrilica">
+        <label for="acrilica">Acrílica</label>
+      </div>
+    </td>
+    
+    <td>
+      <select>
+        <option value="Coche">Coche</option>
+        <option value="Moto">Moto</option>
+        <option value="etc">etc</option>
+      </select>
+    </td>
+    
+    <td>
+      <input class="check" type="checkbox">
+    </td>
+  `
+  tableBody.appendChild(tr)
+
+  tableBody.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+      button.parentElement.parentElement.remove()
+    })
+  })
 }
 
-const eliminarFila = () => {
-  const table = document.getElementById("headtable")
-  const rowCount = table.rows.length
-
-  if (rowCount <= 1) alert("No se puede eliminar encabezado")
-  else table.deleteRow(rowCount - 1)
+const exportToJSON = () => {
+  console.log(tableBody);
 }
